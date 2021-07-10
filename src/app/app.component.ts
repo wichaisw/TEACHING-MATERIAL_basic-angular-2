ea525900d4371ea4ab22b1c276f1b5be27b1089b
wichaisw <wichai.sw19@gmail.com>
2021-07-10 23:50:03 +0700

    init project with login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-to-do-list';
+  username: string = '';
+  password: string = '';
+  
 }
