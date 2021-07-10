2c1ec2c6e622ae1c7ab9ea2e0479841f54f963bb
wichaisw <wichai.sw19@gmail.com>
2021-07-11 01:41:34 +0700

    add router link

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
+  @Input() text:string = '';
 
   constructor() { }
 
